4fb02e61bbce3b9ac4bc1421634648eafb5a7d65
bertrandlf <bertrandlf@gmail.com>
2017-03-17 14:20:19 +0100

    cleanup links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 
 export const ROUTES: Routes = [
-  { path: '',      component: GameComponent },
-  { path: 'highscores',  component: GameComponent },
+  { path: '', component: GameComponent },
+  { path: 'play', component: GameComponent },
+  { path: 'highscores', component: GameComponent },
 ];
