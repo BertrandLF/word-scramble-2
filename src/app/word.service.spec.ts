b5220898d2cb8df5d81d79a125dd18eaec58085c
bertrandlf <bertrandlf@gmail.com>
2017-03-27 16:12:25 +0200

    test wordService

--- src/app/word.service.spec.ts
+++ src/app/word.service.spec.ts
@@ -1,19 +1,36 @@
+import { Guess } from 'app/models/guess.model';
+import { Observable } from 'rxjs/Observable';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { DatabaseService } from 'app/database.service';
 import { firebaseconfig } from './app.module';
 import { AngularFireModule } from 'angularfire2';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { WordService } from './word.service';
+import { Subject } from "rxjs";
 
 describe('WordService', () => {
+
+  const myWords = new Subject();
+  const databaseService = {
+    words: myWords
+  } as DatabaseService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [AngularFireModule.initializeApp(firebaseconfig)],
-      providers: [DatabaseService, WordService]
+      providers: [{ provide: DatabaseService, useValue: databaseService}, WordService]
     });
   });
 
-  it('should ...', inject([WordService], (service: WordService) => {
+  fit('should get a guess from the databaseService', inject([WordService], (service: WordService) => {
     expect(service).toBeTruthy();
+    myWords.next(['Bertrand']);
+    service.guess.subscribe((word: Guess) => {
+      expect(word.solution).toBe('Bertrand');
+      expect(word.guess).not.toBe('Bertrand');
+      expect(word.wordLength).toBe(8);
+    });
+    service.pickAWord();
   }));
 });
